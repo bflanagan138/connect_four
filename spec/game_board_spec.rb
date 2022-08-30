b6f672fdd34beb62cf0b486ebf57d4f206425ebb
Sean Culliton <smculliton@gmail.com>
2022-08-30 14:39:03 -0600

    Update game board test

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe do
         it "has an empty column" do
             game_board = GameBoard.new()
             expect(game_board.a_column.all? { |string| string == "." }).to eq (true)
+            expect(game_board.b_column.all? { |string| string == "." }).to eq (true)
+            expect(game_board.c_column.all? { |string| string == "." }).to eq (true)
+            expect(game_board.d_column.all? { |string| string == "." }).to eq (true)
+            expect(game_board.e_column.all? { |string| string == "." }).to eq (true)
+            expect(game_board.f_column.all? { |string| string == "." }).to eq (true)
+            expect(game_board.g_column.all? { |string| string == "." }).to eq (true)
         end
     end 
 end
