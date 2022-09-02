e02395dcaea5fa43c917b46f501711b8ef26dcf1
Sean Culliton <smculliton@gmail.com>
2022-09-01 19:55:46 -0600

    Add welcome screen art

--- lib/game.rb
+++ lib/game.rb
@@ -11,6 +11,36 @@ class Game
         @player2 = player2 
     end
 
+    def start 
+        puts `clear`
+        puts `clear`
+        puts"                        _____  "
+        puts"                     / /  |  | "
+        puts"                    / /   |  |_"
+        puts"___________________/ /    ^   /"
+        puts" _  _  _  _  _  ____ \\____   | "
+        puts"|_ |_|| || ||/_|_ |       |__|"
+        puts"==============================="
+        puts' '
+        puts '==== Welcome! ==== '
+        puts 'Test your intelligence against our supercomputer.'
+        puts 'Enter <p> to play, <q> to quit'
+
+        user_input = gets.chomp.downcase
+
+        until user_input == 'p' || user_input == 'q' || user_input == '(p)' || user_input == '(q)'
+            puts 'Try again!'
+            user_input = gets.chomp.downcase
+        end 
+
+        if user_input == 'p' || user_input == '(p)'
+            puts `clear`
+            turn
+        elsif user_input == 'q' || user_input == '(q)'
+            puts "Goodbye"
+        end
+    end
+
     #player1 goes first
     #select column
     #check if column valid
@@ -23,15 +53,23 @@ class Game
     #checks for winner
 
     def turn
+        puts @game_board.render_game_board
+        puts ' '
         @game_board.place_game_piece(@player1.select_column, @player1)
-        puts @game_board.render_game_board
         #check_for_winner
-        #game_board_full?
+
         @game_board.place_game_piece(@player2.select_column, @player2)
-        puts @game_board.render_game_board
+
+        # puts @game_board.render_game_board
         #check_for_winner
-        #game_board_full?
-        turn 
+        puts `clear`
+
+        if @game_board.game_board_full?
+            puts 'There are no more moves. Game Over.'
+            #figure out what happens after game over
+        elsif
+            turn
+        end 
     end
 end
 
@@ -40,4 +78,4 @@ player1 = User.new(game_board)
 player2 = Computer.new(game_board)
 game = Game.new(game_board, player1, player2)
 
-game.turn
+game.start
--- lib/game_board.rb
+++ lib/game_board.rb
@@ -8,27 +8,8 @@ class GameBoard
         @e_column = [".", ".", ".", ".", ".", "."]
         @f_column = [".", ".", ".", ".", ".", "."]
         @g_column = [".", ".", ".", ".", ".", "."]
+        @columns = [@a_column, @b_column, @c_column, @d_column, @e_column, @f_column, @g_column]
         #Refactor into individual Cells if necessary (Cell class)
-    end
-
-    def welcome_screen
-        puts `clear`
-        puts "xoxoxoxo Welcome to Connect 4! oxoxoxox" 
-        puts "Test your intelligence against our super computer!"
-        puts "Type (p) to start the game. Type (q) to quit."
-
-        user_input = gets.chomp.downcase
-
-        until user_input == 'p' || user_input == 'q' || user_input == '(p)' || user_input == '(q)'
-            puts 'Try again!'
-            user_input = gets.chomp.downcase
-        end 
-
-        if user_input == 'p' || user_input == '(p)'
-            puts render_game_board
-        elsif user_input == 'q' || user_input == '(q)'
-            puts "Goodbye"
-        end
     end
 
     def valid_column?(column)
@@ -53,4 +34,11 @@ class GameBoard
         end
         render
     end
+
+    def game_board_full?
+        @columns.none? do |column|
+            valid_column?(column)
+        end
+    end 
+        
 end
