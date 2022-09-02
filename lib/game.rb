f2a664f6f536ae26a6fb68e9ab467c1de714991a
Sean Culliton <smculliton@gmail.com>
2022-09-01 18:37:46 -0600

    Initialize game class

---
+++ lib/game.rb
@@ -0,0 +1,13 @@
+class Game 
+    attr_reader :game_board, :player1, :player2
+
+    def initialize(game_board, player1, player2)
+        @game_board = game_board
+        @player1 = player1 
+        @player2 = player2 
+    end
+
+    def turn 
+        
+
+end
